a9d347dfe43d40ad1ebd04eddbe78e5ae8253230
Jordy Salas <jordysp92@gmail.com>
2020-04-11 00:01:15 -0300

    Se agrega template adminpro

---
+++ curso-02-avanzado/ejemplo1/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { AboutComponent } from './pages/about.component';
+import { MainComponent } from './pages/main.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    MainComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
